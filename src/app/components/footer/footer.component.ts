f40941960c3460ec898491a3b099ebf1712ae84b
Felipe Bis <felipe.bis@aluno.ifsp.edu.br>
2023-12-07 11:32:48 -0300

    feat: add footer component and remove duplicate images

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
   standalone: true,
-  imports: [],
+  selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
